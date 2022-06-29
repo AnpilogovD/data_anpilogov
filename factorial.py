d2b72396e7c7d5e263dcebd45ff07a2e0fd2f303
AnpilogovD <dima@lagunat.ru>
2022-06-29 16:51:08 +0400

    1

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    # Задаём условия выхода из рекурсии:
-    if n==0: return 1
-    if n==1: return 1
-    # Во всех других случаях возвращаем
-    # произведение текущего числа n и функции от n-1
-    return factorial(n-1)*n
-print(factorial(322))
